6c641d1bef2ad8117ea6c6f258ac394115debe7c
mratke <mratke@student.42heilbronn.de>
2025-01-28 00:37:44 +0100

    started to build a tree very first protorype without and or im not even sure it works

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 18:38:40 by mratke            #+#    #+#             */
-/*   Updated: 2025/01/27 22:10:10 by mratke           ###   ########.fr       */
+/*   Updated: 2025/01/28 00:35:39 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,59 +21,95 @@ int	is_redirect(char *token)
 	}
 	return (0);
 }
-// t_node	*parse_tokens(t_list **tokens)
-// {
-// 	t_node	*left;
-// 	t_list	*current;
-// 	t_node	*right;
-// 	char	*file;
+// temporary solution because i need an array of strings in command.
+static t_node	*handle_command(t_list **current)
+{
+	int		arg_count;
+	t_list	*temp;
+	char	**args;
+	int		i;
 
-// 	if (!tokens || !*tokens)
-// 		return (NULL);
-// 	left = NULL;
-// 	current = *tokens;
-// 	while (current)
-// 	{
-// 		if (ft_strcmp(current->content, "|") == 0)
-// 		{
-// 			if (!current->next)
-// 				return (left);
-// 			*tokens = current->next;
-// 			right = parse_tokens(tokens);
-// 			left = create_operator_node(PIPE_TYPE, left, right);
-// 			break ;
-// 		}
-// 		else if (is_redirect((char *)current->content))
-// 		{
-// 			char *operator= ft_strdup(current->content);
-// 			if (!current->next)
-// 			{
-// 				free(operator);
-// 				return (left);
-// 			}
-// 			current = current->next;
-// 			file = ft_strdup(current->content);
-// 			left = create_redirect_node(left, operator, file);
-// 			free(operator);
-// 			free(file);
-// 			current = current->next;
-// 		}
-// 		else
-// 		{
-// 			left = handle_command(&current);
-// 			if (!left)
-// 				return (NULL);
-// 		}
-// 	}
-// 	*tokens = current;
-// 	return (left);
-// }
+	i = 0;
+	arg_count = 0;
+	temp = *current;
+	while (temp && !is_redirect(temp->content) && ft_strcmp(temp->content,
+			"|") != 0)
+	{
+		arg_count++;
+		temp = temp->next;
+	}
+	args = malloc((arg_count + 1) * sizeof(char *));
+	while (i < arg_count)
+	{
+		args[i] = ft_strdup((*current)->content);
+		if (!args[i])
+			return (free_double_array(args), NULL);
+		*current = (*current)->next;
+		i++;
+	}
+	args[arg_count] = NULL;
+	return (create_command_node(args));
+}
 
+t_node	*parse_tokens(t_list **tokens)
+{
+	t_node	*left;
+	t_list	*current;
+	t_node	*right;
+	char	*file;
+	char	*oper;
+
+	if (!tokens || !*tokens)
+		return (NULL);
+	left = NULL;
+	current = *tokens;
+	while (current)
+	{
+		if (ft_strcmp(current->content, "|") == 0)
+		{
+			if (!current->next)
+				return (left);
+			*tokens = current->next;
+			right = parse_tokens(tokens);
+			left = create_operator_node(PIPE_TYPE, left, right);
+			break ;
+		}
+		else if (is_redirect((char *)current->content))
+		{
+			oper = ft_strdup(current->content);
+			if (!current->next)
+			{
+				free(oper);
+				return (left);
+			}
+			current = current->next;
+			file = ft_strdup(current->content);
+			left = create_redirect_node(left, oper, file);
+			free(oper);
+			free(file);
+			current = current->next;
+		}
+		else
+		{
+			left = handle_command(&current);
+			if (!left)
+				return (NULL);
+		}
+	}
+	*tokens = current;
+	return (left);
+}
+
+// dont test with && or || not implemented yet
 int	main(void)
 {
-	char	str[100] = "\"echo hi\" | cat << eof";
+	char	str[100] = "\"echo hi\" | cat << eof | echo hello | grep file.txt";
 	t_list	*tokens;
+	t_node	*route;
 
 	tokens = tokenize(str);
 	print_list(tokens);
+	printf("\n");
+	route = parse_tokens(&tokens);
+	print_tree(route, 0);
 }
