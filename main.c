9dbef027137ff157bfa88f1ef36d2de8defd7ea4
mratke <mratke@student.42heilbronn.de>
2025-01-27 22:14:05 +0100

    under production parse function jsut save code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 18:38:40 by mratke            #+#    #+#             */
-/*   Updated: 2025/01/27 21:43:38 by mratke           ###   ########.fr       */
+/*   Updated: 2025/01/27 22:10:10 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,53 @@ int	is_redirect(char *token)
 	}
 	return (0);
 }
+// t_node	*parse_tokens(t_list **tokens)
+// {
+// 	t_node	*left;
+// 	t_list	*current;
+// 	t_node	*right;
+// 	char	*file;
+
+// 	if (!tokens || !*tokens)
+// 		return (NULL);
+// 	left = NULL;
+// 	current = *tokens;
+// 	while (current)
+// 	{
+// 		if (ft_strcmp(current->content, "|") == 0)
+// 		{
+// 			if (!current->next)
+// 				return (left);
+// 			*tokens = current->next;
+// 			right = parse_tokens(tokens);
+// 			left = create_operator_node(PIPE_TYPE, left, right);
+// 			break ;
+// 		}
+// 		else if (is_redirect((char *)current->content))
+// 		{
+// 			char *operator= ft_strdup(current->content);
+// 			if (!current->next)
+// 			{
+// 				free(operator);
+// 				return (left);
+// 			}
+// 			current = current->next;
+// 			file = ft_strdup(current->content);
+// 			left = create_redirect_node(left, operator, file);
+// 			free(operator);
+// 			free(file);
+// 			current = current->next;
+// 		}
+// 		else
+// 		{
+// 			left = handle_command(&current);
+// 			if (!left)
+// 				return (NULL);
+// 		}
+// 	}
+// 	*tokens = current;
+// 	return (left);
+// }
 
 int	main(void)
 {
